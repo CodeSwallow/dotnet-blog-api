39561a68fd46fc92c3bbe35572a75f8e81467f1b
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-31 23:58:36 -0600

    feat: added basic article controller

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,57 @@
+﻿using DotNetAPI.Models;
+using DotNetAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ArticleController
+{
+    public ArticleController() { }
+    
+    [HttpGet]
+    public ActionResult<List<Article>> GetAll() => ArticleService.GetAll();
+    
+    [HttpGet("{id}")]
+    public ActionResult<Article?> Get(int id)
+    {
+        var article = ArticleService.Get(id);
+        if (article is null)
+            return new NotFoundResult();
+        
+        return article;
+    }
+    
+    [HttpPost]
+    public IActionResult Create(Article article)
+    {
+        ArticleService.Add(article);
+        return new CreatedAtRouteResult("Get", new { id = article.Id }, article);
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Article article)
+    {
+        if (id != article.Id)
+            return new BadRequestResult();
+        
+        var existingArticle = ArticleService.Get(id);
+        if (existingArticle is null)
+            return new NotFoundResult();
+        
+        ArticleService.Update(article);
+        return new NoContentResult();
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var article = ArticleService.Get(id);
+        if (article is null)
+            return new NotFoundResult();
+        
+        ArticleService.Delete(id);
+        return new NoContentResult();
+    }
+}
