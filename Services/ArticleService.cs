e9400ac4ae9499cc318f60e392b43d866ad831a8
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-05-31 23:55:45 -0600

    feat: completed basic crud services

--- Services/ArticleService.cs
+++ Services/ArticleService.cs
@@ -4,5 +4,39 @@ namespace DotNetAPI.Services;
 
 public class ArticleService
 {
-    static List<Article> Articles { get; }
+    private static List<Article> Articles { get; }
+    private static int _nextId = 0;
+
+    static ArticleService()
+    {
+        Articles = new List<Article> { };
+    }
+    
+    public static List<Article> GetAll() => Articles;
+    
+    public static Article? Get(int id) => Articles.FirstOrDefault(a => a.Id == id);
+    
+    public static void Add(Article article)
+    {   
+        article.Id = _nextId++;
+        Articles.Add(article);
+    }
+    
+    public static void Delete(int id)
+    {
+        var article = Get(id);
+        if (article is null)
+            return;
+        
+        Articles.Remove(article);
+    }
+    
+    public static void Update(Article article)
+    {
+        var index = Articles.FindIndex(a => a.Id == article.Id);
+        if (index == -1)
+            return;
+        
+        Articles[index] = article;
+    }
 }
